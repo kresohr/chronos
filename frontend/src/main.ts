38a63c0561aa0ef3254289b446d4c3b55d1b6130
kresohr <ilicic.kresimir@gmail.com>
2025-02-16 17:31:54 +0100

    Included fortawesome package in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,18 +6,25 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas,fab,far)
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.component('fa-icon', FontAwesomeIcon)
 app.use(router)
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
+  theme: {
+    preset: Aura,
+  },
 })
-
 
 app.mount('#app')
