d26c2af3f5602e503ae76839658fe98c900b28fc
kresohr <ilicic.kresimir@gmail.com>
2025-02-21 20:38:12 +0100

    Added V_ManageEmployees component

---
+++ frontend/src/views/V_ManageEmployees.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Manage Employees</h1>
+  <p>View employees or add new</p>
+
+  <section>
+    <div class="manage-employees"></div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.manage-employees {
+  margin-top: var(--spacing-large);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-inline: auto;
+  max-width: 240px;
+  gap: var(--spacing-small);
+
+  @media screen and (min-width: 600px) {
+    max-width: 360px;
+  }
+
+  @media screen and (min-width: 900px) {
+    flex-direction: row;
+    max-width: 760px;
+  }
+
+  @media screen and (min-width: 1300px) {
+    max-width: 1000px;
+  }
+}
+</style>
