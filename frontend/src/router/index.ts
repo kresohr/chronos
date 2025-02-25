13de5125d6e3481cad35a4433ced35f5f3bde6fc
kresohr <ilicic.kresimir@gmail.com>
2025-02-25 22:08:22 +0100

    Added route to new employee

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import V_Home from '../views/V_Home.vue'
 import V_AdminPanel from '@/views/V_AdminPanel.vue'
 import V_ManageEmployees from '@/views/V_ManageEmployees.vue'
+import V_NewEmployee from '@/views/V_NewEmployee.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,13 @@ const router = createRouter({
       path: '/manage-employees',
       name: 'manageemployees',
       component: V_ManageEmployees,
-    }
+    },
+    {
+      path: '/manage-employees/new',
+      name: 'newemployee',
+      component: V_NewEmployee,
+    },
+    
   ],
 })
 
