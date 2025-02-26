d88ac55da14eb3c83170ffe0e9ef508704a6331b
kresohr <ilicic.kresimir@gmail.com>
2025-02-26 19:21:25 +0100

    UI-FIX: Disabled Dark Mode, added Toast component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,6 +8,8 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -24,7 +26,12 @@ app.use(router)
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
+    options: {
+      darkModeSelector: false || 'none',
+    }
   },
 })
+app.use(ToastService);
+app.component('Toast', Toast);
 
 app.mount('#app')
