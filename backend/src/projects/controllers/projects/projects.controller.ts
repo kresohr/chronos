fa2ffaa16ba4af1a765842ae9b2c2844f6a60dc5
kresohr <ilicic.kresimir@gmail.com>
2025-03-12 21:06:39 +0100

    Modified projects controller

--- backend/src/projects/controllers/projects/projects.controller.ts
+++ backend/src/projects/controllers/projects/projects.controller.ts
@@ -7,20 +7,20 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateProjectDto } from 'src/projects/dtos/CreateProject.dto';
+import { ProjectsService } from 'src/projects/services/projects/projects.service';
 
 @Controller('projects')
 export class ProjectsController {
+  constructor(private projectsService: ProjectsService) {}
+
   @Get()
   getProjects() {
-    return {
-      msg: 'Projects',
-    };
+    return this.projectsService.fetchProjects();
   }
 
   @Post('')
   @UsePipes(new ValidationPipe())
   createProject(@Body() projectData: CreateProjectDto) {
-    console.log(projectData.name);
-    return {};
+    return this.projectsService.createProject(projectData);
   }
 }
