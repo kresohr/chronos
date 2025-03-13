dcdb4ee47f6b9cadd3e7dcc505e13fe99aafe571
kresohr <ilicic.kresimir@gmail.com>
2025-03-13 23:12:12 +0100

    Added deleteProject method to projects.service.ts

--- backend/src/projects/services/projects/projects.service.ts
+++ backend/src/projects/services/projects/projects.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -11,5 +11,21 @@ export class ProjectsService {
 
   createProject(data: Prisma.ProjectCreateInput) {
     return this.prisma.project.create({ data });
+  }
+
+  async deleteProject(id: number) {
+    try {
+      const deletedProject = await this.prisma.project.delete({
+        where: {
+          id: id,
+        },
+      });
+      return deletedProject;
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(`Project with id ${id} does not exist.`);
+      }
+      throw error;
+    }
   }
 }
