c551f8cc98a397c12dbdd6501f867122cb78ca15
kresohr <ilicic.kresimir@gmail.com>
2025-03-13 23:13:45 +0100

    Added EmployeesModule to app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectsModule } from './projects/projects.module';
-
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [HealthModule, PrismaModule, ProjectsModule],
+  imports: [HealthModule, PrismaModule, ProjectsModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
