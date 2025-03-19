02572fa9a2d4df75cecbf47cdcac0550302a9530
kresohr <ilicic.kresimir@gmail.com>
2025-03-19 21:26:50 +0100

    Imported PrismaModule to users.module.ts

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users/users.service';
 import { UsersController } from './controllers/users/users.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}
