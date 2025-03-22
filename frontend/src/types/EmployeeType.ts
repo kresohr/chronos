19c5b11ebdb4d68ef21bfb2f751acf5cd03013d8
kresohr <ilicic.kresimir@gmail.com>
2025-03-22 14:35:10 +0100

    Added Employee Type interface

---
+++ frontend/src/types/EmployeeType.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id?: number
+  firstName: string
+  lastName: string
+  email: string
+  isAdmin: boolean
+  role?: string
+  project?: string
+  createdAt?: string
+  updatedAt?: string
+}
