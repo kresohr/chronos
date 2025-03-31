bc6bcf15a061258faff3e7ae25cede318dda6207
kresohr <ilicic.kresimir@gmail.com>
2025-03-31 17:59:06 +0200

    Added route to fetch specific user projects

--- backend/src/employees/controller/employees/employees.controller.ts
+++ backend/src/employees/controller/employees/employees.controller.ts
@@ -3,12 +3,15 @@ import {
   Controller,
   Delete,
   Get,
+  Param,
+  ParseIntPipe,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateEmployeeDto } from 'src/employees/dtos/CreateEmployee.dto';
 import { DeleteEmployeeDto } from 'src/employees/dtos/DeleteEmployee.dto';
+import { FetchEmployeeProjectsDto } from 'src/employees/dtos/FetchEmployeeProjects.dto';
 import { EmployeesService } from 'src/employees/service/employees/employees.service';
 
 @Controller('employees')
@@ -28,6 +31,13 @@ export class EmployeesController {
   @Delete()
   @UsePipes(new ValidationPipe())
   deleteEmployee(@Body() employeeData: DeleteEmployeeDto) {
-    return this.employeesService.deleteEmployee(employeeData.id);
+    return this.employeesService.deleteEmployee(employeeData);
+  }
+
+  @Get(':userid/projects/')
+  @UsePipes(new ValidationPipe())
+  getEmployeeProjects(@Param('userid', ParseIntPipe) employeeData: number) {
+    const dto: FetchEmployeeProjectsDto = { userId: employeeData };
+    return this.employeesService.fetchEmployeeProjects(dto);
   }
 }
