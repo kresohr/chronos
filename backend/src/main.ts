dd270962cfc1ebad408ab26c9f8e5b2b20cf6a11
kresohr <ilicic.kresimir@gmail.com>
2025-04-07 22:32:08 +0200

    Added TODO message to add validationpipe globally instead of writing it in each controller

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO: Implement global validationpipe so I don't repeat myself
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
