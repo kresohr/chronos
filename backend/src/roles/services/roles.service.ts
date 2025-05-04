301bd7d7dd1c3aee229e3dfb370485ffc746a149
kresohr <ilicic.kresimir@gmail.com>
2025-05-04 13:22:52 +0200

    Implemented FetchEmployeesWithRole service method

--- backend/src/roles/services/roles.service.ts
+++ backend/src/roles/services/roles.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma } from '@prisma/client';
+import { FetchEmployeesWithRoleDto } from '../dtos/FetchEmployeesWithRole.dto';
+import { DeleteRoleDto } from '../dtos/DeleteRole.dto';
 
 @Injectable()
 export class RolesService {
@@ -14,17 +16,38 @@ export class RolesService {
     return this.prisma.role.create({ data });
   }
 
-  async deleteRole(id: number) {
+  async deleteRole(data: DeleteRoleDto) {
     try {
       const deletedRole = await this.prisma.role.delete({
         where: {
-          id: id,
+          id: data.id,
         },
       });
       return deletedRole;
     } catch (error) {
       if (error.code === 'P2025') {
-        throw new NotFoundException(`Role with id ${id} does not exist.`);
+        throw new NotFoundException(`Role with id ${data.id} does not exist.`);
+      }
+      throw error;
+    }
+  }
+
+  async fetchEmployeesWithRole(data: FetchEmployeesWithRoleDto) {
+    try {
+      const users = await this.prisma.userRole.findMany({
+        where: {
+          roleId: data.roleId,
+        },
+        select: {
+          user: true,
+        },
+      });
+      return users;
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(
+          `Role with id ${data.roleId} does not exist.`,
+        );
       }
       throw error;
     }
