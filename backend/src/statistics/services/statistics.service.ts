18d1c98572d2ddb0bf6a045d38928e2e1c201606
kresohr <ilicic.kresimir@gmail.com>
2025-05-04 20:19:02 +0200

    Implemented statistics service and countAllData method

---
+++ backend/src/statistics/services/statistics.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class StatisticsService {
+  constructor(private prisma: PrismaService) {}
+
+  async countAllData() {
+    try {
+      const [userCount, roleCount, projectCount, entriesCount] =
+        await this.prisma.$transaction([
+          this.prisma.user.count(),
+          this.prisma.role.count(),
+          this.prisma.project.count(),
+          this.prisma.entry.count(),
+        ]);
+
+      return {
+        totalUsers: userCount,
+        totalRoles: roleCount,
+        totalProject: projectCount,
+        totalEntries: entriesCount,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+}
