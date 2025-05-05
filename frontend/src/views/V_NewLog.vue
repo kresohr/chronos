96fc7d1eb1f04452d02117b013a38c006a46004a
kresohr <ilicic.kresimir@gmail.com>
2025-05-05 20:37:53 +0200

    Implemented NewLog component

---
+++ frontend/src/views/V_NewLog.vue
@@ -0,0 +1,163 @@
+<template>
+  <!-- TODO: Guard route if user is not admin -->
+  <h1>Add Log</h1>
+  <p>Keep track of time spent on each project</p>
+
+  <section>
+    <div class="new-log">
+      <Form
+        :initialValues
+        :resolver="zodFormResolver"
+        @submit="onFormSubmit"
+        class="form-container"
+      >
+        <!-- Add conditional render if project is enabled as a module -->
+        <FormField v-slot="$field" name="project" class="form-field">
+          <label for="project-picker" class="new-log__time--label"
+            >1. Select a project from the list</label
+          >
+          <Select
+            id="project-picker"
+            v-model="selectedProject"
+            :options="projects"
+            optionLabel="name"
+            placeholder="Projects"
+            :filter="true"
+            filterPlaceholder="Search available projects"
+          >
+          </Select>
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">
+            {{ $field.error?.message }}
+          </Message>
+        </FormField>
+
+        <FormField v-slot="$field" name="date" class="new-log__time--wrapper">
+          <label for="date" class="new-log__time--label">2. Select the date</label>
+          <DatePicker
+            id="date"
+            class="new-log__time--datepicker"
+            v-model="selectedDate"
+            dateFormat="dd/mm/yy"
+            placeholder="Select the date..."
+          />
+        </FormField>
+
+        <FormField v-slot="$field" name="time" class="new-log__time--wrapper">
+          <label for="time" class="new-log__time--label">3. Enter the amount of hours</label>
+          <DatePicker
+            id="time"
+            class="new-log__time--datepicker"
+            v-model="selectedTime"
+            showTime
+            hourFormat="24"
+            placeholder="Enter the time"
+            fluid
+            time-only
+          />
+        </FormField>
+
+        <Button type="submit" label="Log" />
+      </Form>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { Form, FormField } from '@primevue/forms'
+import Select from 'primevue/select'
+import DatePicker from 'primevue/datepicker'
+import { computed, onMounted, ref } from 'vue'
+import { Button, Message } from 'primevue'
+import { z } from 'zod'
+import { zodResolver } from '@primevue/forms/resolvers/zod'
+import { useToast } from 'primevue/usetoast'
+import { useProjectsStore } from '@/stores/projects'
+
+const projectsStore = useProjectsStore()
+const toast = useToast()
+const zodSchema = z.object({
+  project: z.object({
+    id: z.number(),
+  }),
+})
+
+const zodFormResolver = zodResolver(zodSchema)
+
+const initialValues = {
+  project: '',
+}
+
+const selectedProject = ref()
+const projects = computed(() => projectsStore.allProjects)
+const selectedTime = ref()
+const selectedDate = ref()
+
+const onFormSubmit = async ({ valid, values }: { valid: boolean; values?: any }) => {
+  if (!valid || !values) {
+    toast.add({
+      severity: 'error',
+      summary: 'Form validation failed.',
+      life: 3000,
+    })
+    return
+  }
+
+  // TODO: Implement onFormSubmit method
+}
+
+onMounted(() => {
+  projectsStore.fetchAllProjects()
+})
+</script>
+
+<style scoped lang="scss">
+.new-log {
+  margin-top: var(--spacing-large);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-inline: auto;
+  gap: var(--spacing-small);
+
+  &__time {
+    &--wrapper {
+      display: flex;
+      flex-direction: column;
+      gap: var(--spacing-small);
+    }
+
+    &--label {
+      font-weight: bold;
+    }
+
+    &--datepicker {
+      textarea {
+        text-align: center;
+      }
+      ::placeholder {
+        text-align: center;
+      }
+    }
+  }
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-medium);
+  width: 100%;
+  max-width: 320px;
+}
+
+.form-field {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-xsmall);
+  &--checkbox {
+    display: flex;
+    justify-content: end;
+    gap: var(--spacing-small);
+  }
+}
+</style>
