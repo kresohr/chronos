5bf700d2a9f7665e106813a436278b12cad26229
kresohr <ilicic.kresimir@gmail.com>
2025-05-08 19:56:37 +0200

    WIP: Home component

--- frontend/src/views/V_Home.vue
+++ frontend/src/views/V_Home.vue
@@ -1,8 +1,21 @@
 <template>
-  <h1>This is the home component.</h1>
-  <p>Example paragraph</p>
+  <!-- Much of the stuff is hardcoded here and will be added once login/users is implemented properly with Keycloak -->
+  <h1>Hi, {{ loggedUser }}.</h1>
+  <p>The date is: {{ formattedDate }}</p>
+
+  <p>
+    You <strong>{{ hasLogged ? 'have' : `haven't` }}</strong> logged work hours today.
+  </p>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import dayjs from 'dayjs'
+
+const loggedUser = ref('Name')
+const now = dayjs()
+const formattedDate = now.format('DD MMM YYYY')
+const hasLogged = ref(false)
+</script>
 
 <style scoped></style>
