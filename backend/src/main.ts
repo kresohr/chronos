5d323e52c570c039fdc494f5a9eb7b8d58f00e2a
kresohr <ilicic.kresimir@gmail.com>
2025-06-12 21:04:51 +0200

    Added ValidationPipe to main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // TODO: Implement global validationpipe so I don't repeat myself
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
