b126739bf7b576dd98724558fd67b7037c4c0db2
kresohr <ilicic.kresimir@gmail.com>
2025-06-12 23:08:33 +0200

    WIP: Implemented tables for logged hours

--- frontend/src/views/V_NewLog.vue
+++ frontend/src/views/V_NewLog.vue
@@ -55,23 +55,82 @@
 
         <Button :disabled="isDataInvalid" type="submit" label="Log" />
       </Form>
+      <DataTable
+        :value="allEmployeeEntries"
+        :rows="5"
+        :paginator="true"
+        :style="[{ width: '100%' }]"
+        v-model:filters="filters"
+        v-if="allEmployeeEntries.length > 0"
+        class="new-log__hours-logged"
+      >
+        <template #header>
+          <div class="new-log__search">
+            <IconField>
+              <InputIcon>
+                <i class="pi pi-search" />
+              </InputIcon>
+              <InputText v-model="filters['global'].value" placeholder="Search..." />
+            </IconField>
+          </div>
+        </template>
+        <Column field="project.name" header="Project" sortable style="text-align: center"></Column>
+        <Column field="timeLogged" header="Project" sortable>
+          <template #body="slotProps">
+            {{ formatMinutesToHHmm(slotProps.data.timeLogged) }}
+          </template>
+        </Column>
+
+        <!-- TODO: Conditionally render if the person is admin & module is enabled -->
+        <!-- TODO: Implement removal -->
+        <Column v-if="true" class="projects__edit-column" field="edit" header="Remove">
+          <template #body="data">
+            <router-link
+              :to="`/projects/manage/${data.data.id}`"
+              class="projects__icon-button--link"
+            >
+              <button class="new-log__remove-button" @click="console.log('To implement removal.')">
+                <fa-icon :icon="['fas', 'xmark']" style="height: 20px; width: 20px" />
+              </button>
+            </router-link>
+          </template>
+        </Column>
+
+        <ColumnGroup type="footer">
+          <Row>
+            <Column footer="Total Hours Tracked:" footerStyle="text-align:right" />
+            <Column :footer="employeeEntriesTotalHours" footerStyle="text-align:center" />
+            <Column />
+          </Row>
+        </ColumnGroup>
+      </DataTable>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { Form, FormField } from '@primevue/forms'
+import Column from 'primevue/column'
+import DataTable from 'primevue/datatable'
+import Row from 'primevue/row'
+import ColumnGroup from 'primevue/columngroup'
+import { FilterMatchMode } from '@primevue/core/api'
 import Select from 'primevue/select'
 import DatePicker from 'primevue/datepicker'
 import CustomTimeInput from '@/components/C_CustomTimeInput.vue'
-import { computed, onMounted, ref } from 'vue'
-import { Button, Message } from 'primevue'
+import { computed, onMounted, ref, watch } from 'vue'
+import { Button, Message, IconField, InputIcon, InputText } from 'primevue'
 import { z } from 'zod'
 import { zodResolver } from '@primevue/forms/resolvers/zod'
 import { useToast } from 'primevue/usetoast'
 import { useProjectsStore } from '@/stores/projects'
+import { useEntriesStore } from '@/stores/entries'
+import { formatMinutesToHHmm } from '@/utils/formatMinutesToHHmm'
 
 const projectsStore = useProjectsStore()
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+})
 const toast = useToast()
 const zodSchema = z.object({
   project: z.object({
@@ -92,6 +151,14 @@ const projects = computed(() => projectsStore.allProjects)
 const selectedDate = ref()
 const selectedHour = ref(8)
 const selectedMinutes = ref(0)
+const entriesStore = useEntriesStore()
+const allEmployeeEntries = computed(() => entriesStore.allEmployeeEntries)
+/* TODO: Filter for selected date hours only */
+const employeeEntriesTotalHours = computed(() => {
+  return formatMinutesToHHmm(
+    allEmployeeEntries.value.reduce((accumulator, entry) => accumulator + entry.timeLogged, 0),
+  )
+})
 
 const isDataInvalid = computed((): boolean => {
   if (selectedHour.value === 0 && selectedMinutes.value === 0) {
@@ -124,12 +191,13 @@ const onFormSubmit = async ({ valid, values }: { valid: boolean; values?: any })
       life: 3000,
     })
   } else {
-    // TODO: Implement onFormSubmit method
+    console.log('POO')
   }
 }
 
 onMounted(() => {
   projectsStore.fetchAllProjects()
+  entriesStore.fetchEmployeeEntries(8)
 })
 </script>
 
@@ -162,6 +230,25 @@ onMounted(() => {
         text-align: center;
       }
     }
+  }
+
+  &__remove-button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    :hover {
+      transition: color 0.2s;
+      color: var(--primary-button-color);
+    }
+  }
+
+  &__search {
+    display: flex;
+    justify-content: end;
+  }
+
+  &__hours-logged {
+    margin-top: var(--spacing-large);
   }
 }
 
