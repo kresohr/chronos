3bb9bbf621152e5784ba16db81548673bd33e1f1
kresohr <ilicic.kresimir@gmail.com>
2025-06-12 21:06:58 +0200

    Added controller routes for entries

--- backend/src/entries/controllers/entries.controller.ts
+++ backend/src/entries/controllers/entries.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { EntriesService } from '../services/entries.service';
+import { FetchEmployeeEntriesDto } from '../dtos/FetchEmployeeEntries.dto';
+import { CreateEntryDto } from '../dtos/CreateEntry.dto';
 
 @Controller('entries')
-export class EntriesController {}
+export class EntriesController {
+  constructor(private entriesService: EntriesService) {}
+
+  @Get(':id')
+  @UsePipes(new ValidationPipe())
+  getEntries(@Param('id', ParseIntPipe) userId: number) {
+    const dto: FetchEmployeeEntriesDto = { userId };
+    return this.entriesService.fetchEntriesForUser(dto);
+  }
+
+  @Post('/new')
+  @UsePipes(new ValidationPipe())
+  createEntry(@Body() entryData: CreateEntryDto) {
+    return this.entriesService.createLog(entryData);
+  }
+}
